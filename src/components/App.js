2ad59f94944ce883186a74a529226df4985191b3
dianeyasminec <dianeyasminec60@gmail.com>
2021-10-15 11:25:40 -0400

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React,{useState} from "react";
 import CategoryFilter from "./CategoryFilter";
 import NewTaskForm from "./NewTaskForm";
 import TaskList from "./TaskList";
@@ -7,24 +7,31 @@ import { CATEGORIES, TASKS } from "../data";
 console.log("Here's the data you're working with");
 console.log({ CATEGORIES, TASKS });
 // use .map to render list of categories and task using key prop
-function renderTask(props) {
-const TASKS = props.TASKS
-const ListTask = TASKS.map(task => task)
-console.log(ListTask)
-}
+// function renderTask(props) {
+// const TASKS = props.TASKS
+// const ListTask = TASKS.map(task => task)
+// console.log(ListTask)
+// }
 
 // const ListCategory = CATEGORIES.map((categorie)=> categorie)
 //     console.log(ListCategory)
 
 function App() {
+const [ tasks, setTasks]= useState(TASKS)
+const[categories,setCategories] = useState("All")
+function addTask(task) {
 
+}
+function handleDeleteTask(task) {
+const deleteTask = TASKS.filter(task => task.id != id)
+}
 
     //console.log(CATEGORIES.map(category => category))
   
   return (
     <div className="App">
       <h2>My tasks</h2>
-      <CategoryFilter />
+      <CategoryFilter  Category={CATEGORIES} selectedCategory={categories} setCategories={setCategories}/>
       <NewTaskForm />
       <TaskList />
     </div>
--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,10 +1,12 @@
 import React from "react";
 
+
+
 function TaskList() {
   return (
     <div className="tasks">
-      <li>key={/* display a list of tasks using Task component */
-      ListTask.id}
+      <li>{/* display a list of tasks using Task component */}
+     
       </li>
     </div>
   );
